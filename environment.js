825893fc1d91499f4b0837dc1ac40a2d1b2e7f52
Aliin S <113007378+aliinS@users.noreply.github.com>
2023-05-12 13:13:38 +0300

    Delete environment.js

--- environment.js
+++
@@ -1,7 +0,0 @@
-const API_KEY = "671af4f62fmsh00b483cf4a8c8cep19e825jsn5837a5508baa";
-
-const BASE_URL = "https://coinranking1.p.rapidapi.com";
-
-const RAPID_API_HOST = 'coinranking1.p.rapidapi.com';
-
-export { API_KEY, BASE_URL, RAPID_API_HOST};
