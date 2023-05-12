7d13044fa9306a3787f0320d7349767b84ba9544
Aliin S <113007378+aliinS@users.noreply.github.com>
2023-05-12 12:42:54 +0300

    Add files via upload

---
+++ environment.js
@@ -0,0 +1,7 @@
+const API_KEY = "671af4f62fmsh00b483cf4a8c8cep19e825jsn5837a5508baa";
+
+const BASE_URL = "https://coinranking1.p.rapidapi.com";
+
+const RAPID_API_HOST = 'coinranking1.p.rapidapi.com';
+
+export { API_KEY, BASE_URL, RAPID_API_HOST};
---
+++ src/coin.js
@@ -0,0 +1,70 @@
+import { getCoins, getCoin, getHistory } from "../api/coinService.js";
+
+
+const data = await getCoins();
+
+
+const marketTrendsWrapper = document.querySelector("#market_trends");
+
+
+
+data.data.coins.forEach((coin) => {
+    const article = document.createElement('article');
+
+article.setAttribute("data-uuid", coin.uuid)
+    const articleClasses = [
+        "border-2", 
+        "p-4",  
+        "rounded-2xl",
+    ];
+
+    const borderColorsWeDontWant = ["#000000"];
+
+    article.style.borderColor = borderColorsWeDontWant.includes(coin.color) 
+            ? "#73FDAA" 
+            : coin.color; 
+
+    //teeb border colori värviliseks. Võtab vastava värvi fetchitud failist. Ja kui on must, siis muudab värvi ära. ?=if
+    // kui on true, siis lähen see värv 73fdaa sinna asemele, kui on false, siis lähen coin.color
+
+    article.classList.add(...articleClasses);
+    article.innerHTML = `
+    
+    
+        <div class="flex mb-2">
+            <div class="flex place-items-center justify-around w-2/3">
+                <img class="w-12 h-12 object-cover" src="${coin.iconUrl}" alt="${coin.name}">
+                <p>${coin.symbol}</p>
+                <p class="text-[1.5vh] opacity-70 truncate">${coin.name}</p>
+            </div>
+            <img class="mx-auto mt-4 h-4 v-4" src="/src/images/Vector 3.png" alt="">
+        </div>
+        <div class="grid gap-2 px-6 pt-6 pb-2">
+            <p class="text-2xl">${Number(coin.price).toFixed(2)}</p>
+            <p class="${String(coin.change).startsWith('-') 
+            ? 'text-red-500' 
+            : 'text-green-500'
+        }">${coin.change}%</p>
+        </div>
+    `;
+
+
+    marketTrendsWrapper.append(article);
+});
+
+const cards = document.querySelectorAll("#market_trends article");
+
+cards.forEach((node) => {
+
+    node.addEventListener('click', async (event) => {
+        const uuid = event.currentTarget.getAttribute("data-uuid");
+        
+        const response = await getCoin(uuid);
+        const history = await getHistory(uuid);
+
+        const result = {...response.data.coin, ...history.data}; 
+
+        console.log(result)
+    });
+});
+
